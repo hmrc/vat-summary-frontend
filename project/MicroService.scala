cc4cc600437585b8b5c8c42e08b6d00120248838
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2017-09-13 16:50:24 +0100

    Added separate logging for integration tests

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -91,6 +91,10 @@ private object TestPhases {
 
   def oneForkedJvmPerTest(tests: Seq[TestDefinition]): Seq[Group] =
     tests map {
-      test => new Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
+      test => new Group(
+        test.name,
+        Seq(test),
+        SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name, "-Dlogger.resource=logback-test.xml")))
+      )
     }
 }
