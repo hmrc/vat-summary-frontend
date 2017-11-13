a827ff3b04800d99341db920fd544d339127f9c5
jack-green <jack.green@digital.hmrc.gov.uk>
2017-11-13 12:51:52 +0000

    Removed main_template in BTA partial

---
+++ test/views/partials/BtaPartialViewSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.partials
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import views.ViewBaseSpec
+
+class BtaPartialViewSpec extends ViewBaseSpec {
+
+  "Rendering the BTA partial" should {
+
+    object Selectors {
+      val pageHeading = "h1"
+      val instructions = "p"
+    }
+
+    lazy val view = views.html.partials.btaPartial()
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    "have the correct document title" in {
+      document.title shouldBe ""
+    }
+
+    "have the correct page heading" in {
+      elementText(Selectors.pageHeading) shouldBe "BTA Partial"
+    }
+
+    "have the correct instructions on the page" in {
+      elementText(Selectors.instructions) shouldBe "BTA Partial"
+    }
+  }
+}
