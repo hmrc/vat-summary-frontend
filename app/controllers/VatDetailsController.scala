bde5a19f52594af56a7823295a6a8049d3a7cc37
lewis-hay <lewis.hay@digital.hmrc.gov.uk>
2017-12-18 16:33:42 +0000

    Removed unused import

--- app/controllers/VatDetailsController.scala
+++ app/controllers/VatDetailsController.scala
@@ -18,7 +18,7 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import config.{AppConfig, VatHeaderCarrierForPartialsConverter}
+import config.AppConfig
 import models.{User, VatDetailsModel}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, Request, Result}
