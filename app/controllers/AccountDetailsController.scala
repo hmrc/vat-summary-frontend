64e7b9d86abe7f544338bf4f5b3166760ec75d72
ja-green <jack.green@digital.hmrc.gov.uk>
2018-01-18 16:39:46 +0000

    Added Account Details page

---
+++ app/controllers/AccountDetailsController.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import javax.inject.Inject
+
+import config.AppConfig
+import models.User
+import models.viewModels.AccountDetailsModel
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import services.{BtaHeaderPartialService, EnrolmentsAuthService, VatDetailsService}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
+
+class AccountDetailsController @Inject()(val messagesApi: MessagesApi,
+                                         val enrolmentsAuthService: EnrolmentsAuthService,
+                                         btaHeaderPartialService: BtaHeaderPartialService,
+                                         implicit val appConfig: AppConfig,
+                                         vatDetailsService: VatDetailsService)
+  extends AuthorisedController with I18nSupport {
+
+  def accountDetails(): Action[AnyContent] = authorisedAction { implicit request =>
+    user =>
+      for {
+        accountModel <- handleAccountDetailsModel(user)
+      } yield Ok(views.html.account.accountDetails(user, accountModel))
+  }
+
+  private[controllers] def handleAccountDetailsModel(user: User)(implicit hc: HeaderCarrier): Future[AccountDetailsModel] = {
+    Future.successful(
+      AccountDetailsModel(
+        "Betty Jones",
+        "Bedrock Quarry, Bedrock, Graveldon",
+        "GV2 4BB",
+        "13 Pebble lane, Bedrock, Graveldon",
+        "GV13 4BJ",
+        "01632 982028",
+        "07700 900018",
+        "01632 960026",
+        "bettylucknexttime@gmail.com"
+      )
+    )
+  }
+}
