69d395805b3c19bd2e60502480abf032fdffd891
ja-green <jack.green@digital.hmrc.gov.uk>
2018-01-19 14:20:32 +0000

    Added tests for account details page

--- app/controllers/AccountDetailsController.scala
+++ app/controllers/AccountDetailsController.scala
@@ -23,16 +23,14 @@ import models.User
 import models.viewModels.AccountDetailsModel
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
-import services.{BtaHeaderPartialService, EnrolmentsAuthService, VatDetailsService}
+import services.EnrolmentsAuthService
 import uk.gov.hmrc.http.HeaderCarrier
 
 import scala.concurrent.Future
 
 class AccountDetailsController @Inject()(val messagesApi: MessagesApi,
                                          val enrolmentsAuthService: EnrolmentsAuthService,
-                                         btaHeaderPartialService: BtaHeaderPartialService,
-                                         implicit val appConfig: AppConfig,
-                                         vatDetailsService: VatDetailsService)
+                                         implicit val appConfig: AppConfig)
   extends AuthorisedController with I18nSupport {
 
   def accountDetails(): Action[AnyContent] = authorisedAction { implicit request =>
---
+++ test/views/account/AccountDetailsViewSpec.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.account
+
+import models.User
+import models.viewModels.AccountDetailsModel
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import views.ViewBaseSpec
+
+class AccountDetailsViewSpec extends ViewBaseSpec {
+
+  object Selectors {
+    val pageHeading = "h1"
+    val editAccountHeading = "div#edit-details p"
+
+    val businessDetailsHeading = "h2:nth-of-type(1)"
+    val contactDetailsHeading = "h2:nth-of-type(2)"
+
+    val nameRow = "dt#name"
+    val businessAddressRow = "dt#business-address"
+    val businessContactRow = "dt#business-contact"
+    val businessPhoneRow = "div#business-phone"
+    val businessMobileRow = "div#business-mobile"
+    val correspondenceAddressRow = "dt#correspondence-address"
+    val correspondenceContactRow = "dt#correspondence-contact"
+    val correspondenceEmailRow = "div#correspondence-email"
+    val correspondencePhoneRow = "div#correspondence-phone"
+    val correspondenceMobileRow = "div#correspondence-mobile"
+  }
+
+  private val user = User("1111")
+  val account = AccountDetailsModel("", "", "", "", "", "", "", "", "")
+
+  "Rendering the VAT details page" should {
+
+    lazy val view = views.html.account.accountDetails(user, account)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    "have the correct document title" in {
+      document.title shouldBe "Account details"
+    }
+
+    "have the correct page heading" in {
+      elementText(Selectors.pageHeading) shouldBe "Account details"
+    }
+
+    "have the correct sub heading" in {
+      elementText(Selectors.editAccountHeading) shouldBe "You edit these details in your accounting software."
+    }
+
+    "have the business details section" in {
+      elementText(Selectors.businessDetailsHeading) shouldBe "Business details"
+    }
+
+    "have the contact details section" in {
+      elementText(Selectors.contactDetailsHeading) shouldBe "Contact details"
+    }
+
+    "have the name heading" in {
+      elementText(Selectors.nameRow) shouldBe "Name"
+    }
+
+    "have the business address heading" in {
+      elementText(Selectors.businessAddressRow) shouldBe "Business address"
+    }
+
+    "have the business contact details heading" in {
+      elementText(Selectors.businessContactRow) shouldBe "Business contact details"
+    }
+
+    "have the business phone heading" in {
+      elementText(Selectors.businessPhoneRow) shouldBe "Business phone"
+    }
+
+    "have the business mobile phone heading" in {
+      elementText(Selectors.businessMobileRow) shouldBe "Mobile phone"
+    }
+
+    "have the correspondence address heading" in {
+      elementText(Selectors.correspondenceAddressRow) shouldBe "Correspondence address"
+    }
+
+    "have the contact details heading" in {
+      elementText(Selectors.correspondenceContactRow) shouldBe "Contact details"
+    }
+
+    "have the email heading" in {
+      elementText(Selectors.correspondenceEmailRow) shouldBe "Email"
+    }
+
+    "have the home phone heading" in {
+      elementText(Selectors.correspondencePhoneRow) shouldBe "Home phone"
+    }
+
+    "have the mobile phone heading" in {
+      elementText(Selectors.correspondenceMobileRow) shouldBe "Mobile phone"
+    }
+  }
+}
