35222b69988694ec0ebc2477997a220dc8e494a3
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2018-03-02 17:07:35 +0000

    Updated view to only show payment option when payments are allowed

--- test/mocks/MockAppConfig.scala
+++ test/mocks/MockAppConfig.scala
@@ -38,6 +38,7 @@ class MockAppConfig(val runModeConfiguration: Configuration, val mode: Mode = Mo
   override val signInUrl: String = ""
   override val signInContinueBaseUrl: String = ""
   override val features: Features = new Features(runModeConfiguration)
+  features.allowPayments(true)
   override val vatSummaryPartial: String = ""
   override val vatSubmittedReturnsUrl: String = "returns-url"
   override val vatReturnDeadlinesUrl: String = ""
