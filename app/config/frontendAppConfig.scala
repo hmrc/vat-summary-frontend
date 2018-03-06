992ca03d346d13b9ce61747b72a5c7ad01bac6b8
Gary Hull <gary.hull@digital.hmrc.gov.uk>
2018-03-06 11:58:49 +0000

    create feedback controller

--- app/config/frontendAppConfig.scala
+++ app/config/frontendAppConfig.scala
@@ -22,6 +22,8 @@ import javax.inject.{Inject, Singleton}
 
 import config.features.Features
 import config.{ConfigKeys => Keys}
+import controllers.feedback.routes
+import controllers.routes
 import play.api.Mode.Mode
 import play.api.mvc.Call
 import play.api.{Configuration, Environment}
@@ -52,6 +54,8 @@ trait AppConfig extends ServicesConfig {
   val paymentsVatUrl: String
   val paymentsReturnUrl: String
   val btaVatOverviewUrl: String
+  val feedbackThankYouPartialUrl: String
+  val feedbackHmrcSubmitPartialUrl: String
 }
 
 @Singleton
@@ -67,6 +71,11 @@ class FrontendAppConfig @Inject()(val runModeConfiguration: Configuration, val e
   override lazy val analyticsHost: String = getString(Keys.googleAnalyticsHost)
   override lazy val reportAProblemPartialUrl: String = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   override lazy val reportAProblemNonJSUrl: String = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
+  override lazy val feedbackThankYouPartialUrl: String = s"$contactHost/contact/beta-feedback/form/confirmation"
+  override lazy val feedbackHmrcSubmitPartialUrl: String = s"$contactHost/contact/beta-feedback/form?resubmitUrl"
+   config.                                        contactFrontendService}/contact/beta-feedback/form/?submitUrl
+
+feedbackFormPartialUrl
 
   private def whitelistConfig(key: String): Seq[String] = Some(new String(Base64.getDecoder
     .decode(runModeConfiguration.getString(key).getOrElse("")), "UTF-8"))
