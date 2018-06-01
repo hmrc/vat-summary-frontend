05e75b33f722ff2be23e2d9f2cc1f9c13b544813
geoffwatson <geoff.watson@digital.hmrc.gov.uk>
2018-06-01 16:47:23 +0100

    direct-debit connector WIP

---
+++ app/controllers/DDController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import audit.AuditingService
+import config.AppConfig
+import connectors.DirectDebitConnector
+import javax.inject.{Inject, Singleton}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import services.EnrolmentsAuthService
+
+@Singleton
+class DDController @Inject()(val messagesApi: MessagesApi,
+                             val enrolmentsAuthService: EnrolmentsAuthService,
+                             ddConnector: DirectDebitConnector,
+                             implicit val appConfig: AppConfig,
+                             auditingService: AuditingService)
+  extends AuthorisedController with I18nSupport {
+
+  def directDebits(): Action[AnyContent] = authorisedAction { implicit request =>
+
+    user => ddConnector.startJourney(user.vrn).map{ url => Redirect(url)}
+  }
+}
---
+++ test/connectors/DirectDebitConnectorSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import controllers.ControllerBaseSpec
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+
+
+
+class DirectDebitConnectorSpec extends ControllerBaseSpec {
+
+  "DirectDebitConnector" should {
+    "generate the correct payments url" in {
+      val connector = new DirectDebitConnector(mock[HttpClient], mockAppConfig)
+      connector.setupUrl shouldEqual "direct-debit-backend/start-journey"
+    }
+  }
+
+
+}
