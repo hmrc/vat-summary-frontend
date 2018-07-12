33ebfc5c7336cc03c95cbab39ecab2f70860da6e
Gary Hull <gary.hull@digital.hmrc.gov.uk>
2018-07-12 10:33:52 +0100

    added schemas it tests

--- it/testOnly/stubs/DynamicStub.scala
+++ it/testOnly/stubs/DynamicStub.scala
@@ -8,12 +8,20 @@ import play.api.http.Status._
 object DynamicStub extends WireMockMethods {
 
   private val clearDataUri = "/setup/all-data"
+  private val clearSchemasUri = "/setup/all-schemas"
   private val populateDataUri = "/setup/data"
+  private val populateSchemaUri = "/setup/schema"
 
   def clearStubOkResponse(): StubMapping = when(method = DELETE, uri = clearDataUri)
     .thenReturn(OK)
 
   def populateStubOkResponse(): StubMapping = when(method = POST, uri = populateDataUri)
+    .thenReturn(OK)
+
+  def clearSchemasOkResponse(): StubMapping = when(method = DELETE, uri = clearSchemasUri)
+    .thenReturn(OK)
+
+  def populateSchemaOkResponse(): StubMapping = when(method = POST, uri = populateSchemaUri)
     .thenReturn(OK)
 
 }
