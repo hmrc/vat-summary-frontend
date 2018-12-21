b22a1a1ec8113d0c92a80b70330ec5638972fe69
Ladon Jackson <ladon.jackson@digital.hmrc.gov.uk>
2018-12-21 14:51:52 +0000

    fixed tests

---
+++ test/models/payments/ChargeTypeSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.payments
+
+import uk.gov.hmrc.play.test.UnitSpec
+
+class ChargeTypeSpec extends UnitSpec {
+
+  "Charge type apply method" when {
+
+    s"given a valid charge type" should {
+      "return the correct ChargeType model" in {
+        ChargeType.apply(ReturnDebitCharge.value) shouldBe ReturnDebitCharge
+        ChargeType.apply(ReturnCreditCharge.value) shouldBe ReturnCreditCharge
+        ChargeType.apply(OACharge.value) shouldBe OACharge
+        ChargeType.apply(OACreditCharge.value) shouldBe OACreditCharge
+        ChargeType.apply(OADebitCharge.value) shouldBe OADebitCharge
+        ChargeType.apply(DefaultSurcharge.value) shouldBe DefaultSurcharge
+        ChargeType.apply(CentralAssessmentCharge.value) shouldBe CentralAssessmentCharge
+        ChargeType.apply(ErrorCorrectionCharge.value) shouldBe ErrorCorrectionCharge
+        ChargeType.apply(ErrorCorrectionCreditCharge.value) shouldBe ErrorCorrectionCreditCharge
+        ChargeType.apply(ErrorCorrectionDebitCharge.value) shouldBe ErrorCorrectionDebitCharge
+        ChargeType.apply(RepaymentSupplement.value) shouldBe RepaymentSupplement
+        ChargeType.apply(RepaySupplement.value) shouldBe RepaySupplement
+        ChargeType.apply(OADefaultInterestCharge.value) shouldBe OADefaultInterestCharge
+        ChargeType.apply(AACharge.value) shouldBe AACharge
+        ChargeType.apply(AAInterestCharge.value) shouldBe AAInterestCharge
+        ChargeType.apply(AAFurtherInterestCharge.value) shouldBe AAFurtherInterestCharge
+        ChargeType.apply(BnpRegPre2010Charge.value) shouldBe BnpRegPre2010Charge
+        ChargeType.apply(BnpRegPost2010Charge.value) shouldBe BnpRegPost2010Charge
+        ChargeType.apply(FtnEachPartnerCharge.value) shouldBe FtnEachPartnerCharge
+        ChargeType.apply(FtnMatPost2010Charge.value) shouldBe FtnMatPost2010Charge
+        ChargeType.apply(FtnMatPre2010Charge.value) shouldBe FtnMatPre2010Charge
+        ChargeType.apply(MiscPenaltyCharge.value) shouldBe MiscPenaltyCharge
+        ChargeType.apply(MpPre2009Charge.value) shouldBe MpPre2009Charge
+        ChargeType.apply(MpRepeatedPre2009Charge.value) shouldBe MpRepeatedPre2009Charge
+        ChargeType.apply(CivilEvasionPenaltyCharge.value) shouldBe CivilEvasionPenaltyCharge
+      }
+    }
+
+    s"given an invalid charge type" should {
+      "throw RuntimeException(Invalid Charge Type)" in {
+        val exception = intercept[Exception] {
+          ChargeType.apply("Bad Charge Type")
+        }
+        exception.getMessage shouldBe "Invalid Charge Type"
+      }
+    }
+  }
+
+  "ChargeType unnaply method" should {
+    "return the correct value for each chargeType" in {
+      ChargeType.unapply(ReturnDebitCharge) shouldBe ReturnDebitCharge.value
+      ChargeType.unapply(ReturnCreditCharge) shouldBe ReturnCreditCharge.value
+      ChargeType.unapply(OACharge) shouldBe OACharge.value
+      ChargeType.unapply(OACreditCharge) shouldBe OACreditCharge.value
+      ChargeType.unapply(OADebitCharge) shouldBe OADebitCharge.value
+      ChargeType.unapply(DefaultSurcharge) shouldBe DefaultSurcharge.value
+      ChargeType.unapply(CentralAssessmentCharge) shouldBe CentralAssessmentCharge.value
+      ChargeType.unapply(ErrorCorrectionCharge) shouldBe ErrorCorrectionCharge.value
+      ChargeType.unapply(ErrorCorrectionCreditCharge) shouldBe ErrorCorrectionCreditCharge.value
+      ChargeType.unapply(ErrorCorrectionDebitCharge) shouldBe ErrorCorrectionDebitCharge.value
+      ChargeType.unapply(RepaymentSupplement) shouldBe RepaymentSupplement.value
+      ChargeType.unapply(RepaySupplement) shouldBe RepaySupplement.value
+      ChargeType.unapply(OADefaultInterestCharge) shouldBe OADefaultInterestCharge.value
+      ChargeType.unapply(AACharge) shouldBe AACharge.value
+      ChargeType.unapply(AAInterestCharge) shouldBe AAInterestCharge.value
+      ChargeType.unapply(AAFurtherInterestCharge) shouldBe AAFurtherInterestCharge.value
+      ChargeType.unapply(BnpRegPre2010Charge) shouldBe BnpRegPre2010Charge.value
+      ChargeType.unapply(BnpRegPost2010Charge) shouldBe BnpRegPost2010Charge.value
+      ChargeType.unapply(FtnEachPartnerCharge) shouldBe FtnEachPartnerCharge.value
+      ChargeType.unapply(FtnMatPost2010Charge) shouldBe FtnMatPost2010Charge.value
+      ChargeType.unapply(FtnMatPre2010Charge) shouldBe FtnMatPre2010Charge.value
+      ChargeType.unapply(MiscPenaltyCharge) shouldBe MiscPenaltyCharge.value
+      ChargeType.unapply(MpPre2009Charge) shouldBe MpPre2009Charge.value
+      ChargeType.unapply(MpRepeatedPre2009Charge) shouldBe MpRepeatedPre2009Charge.value
+      ChargeType.unapply(CivilEvasionPenaltyCharge) shouldBe CivilEvasionPenaltyCharge.value
+    }
+  }
+}
---
+++ test/models/payments/PaymentSpec.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.payments
+
+import java.time.LocalDate
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.play.test.UnitSpec
+
+class PaymentSpec extends UnitSpec {
+
+  "paymentReads" when {
+
+    val startDate = "2017-01-01"
+    val endDate = "2017-03-01"
+    val dueDate = "2017-03-08"
+
+    "given a start and end date" should {
+
+      "write to Json correctly" in {
+        val paymentJson = Json.obj(
+          "chargeType" -> ReturnDebitCharge.value,
+          "taxPeriodFrom" -> startDate,
+          "taxPeriodTo" -> endDate,
+          "items" -> Json.arr(
+            Json.obj(
+              "dueDate" -> dueDate
+            )
+          ),
+          "outstandingAmount" -> 9999,
+          "periodKey" -> "#001"
+        )
+
+        val paymentWithPeriodModel = PaymentWithPeriod(
+          chargeType = ReturnDebitCharge,
+          start = LocalDate.parse(startDate),
+          end = LocalDate.parse(endDate),
+          due = LocalDate.parse(dueDate),
+          outstandingAmount = 9999,
+          periodKey = "#001"
+        )
+        paymentJson.as[Payment] shouldBe paymentWithPeriodModel
+      }
+    }
+
+    "not given a start and end date" should {
+
+      "write to Json correctly" in {
+        val paymentJson = Json.obj(
+          "chargeType" -> ReturnDebitCharge.value,
+          "items" -> Json.arr(
+            Json.obj(
+              "dueDate" -> dueDate
+            )
+          ),
+          "outstandingAmount" -> -9999,
+          "periodKey" -> "#001"
+        )
+
+        val paymentNoPeriodModel = PaymentNoPeriod(
+          chargeType = ReturnDebitCharge,
+          due = LocalDate.parse(dueDate),
+          outstandingAmount = -9999,
+          periodKey = "#001"
+        )
+        paymentJson.as[Payment] shouldBe paymentNoPeriodModel
+      }
+    }
+
+    "not given only a start or an end date" should {
+
+      "write to Json correctly" in {
+        val paymentJson = Json.obj(
+          "chargeType" -> ReturnDebitCharge.value,
+          "taxPeriodFrom" -> startDate,
+          "items" -> Json.arr(
+            Json.obj(
+              "dueDate" -> dueDate
+            )
+          ),
+          "outstandingAmount" -> 9999,
+          "periodKey" -> "#001"
+        )
+
+        val exception = intercept[Exception] {
+          paymentJson.as[Payment]
+        }
+        exception.getMessage shouldBe s"Partial taxPeriod was supplied: start: 'Some($startDate)', end: 'None'"
+      }
+    }
+
+  }
+
+}
