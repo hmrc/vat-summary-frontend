0abfd751c82ed749f4ef08f61d4ab430f6dbf8a5
paulfirmstone <35600581+paulfirmstone@users.noreply.github.com>
2020-09-18 10:11:25 +0100

    Updating Welsh content for Accessibility statement

--- test/views/AccessibilityStatementViewSpec.scala
+++ test/views/AccessibilityStatementViewSpec.scala
@@ -176,8 +176,8 @@ class AccessibilityStatementViewSpec extends ViewBaseSpec {
               "disabled, the menu toggle is not hidden " +
             "On some pages the hints and error hints may not read out correctly when using a screen reader " +
             "On some pages the language selector and back links cannot be skipped using assistive technologies " +
-            "On pages ‘Your business details‘ and ‘Confirm your email address‘ the back options are not consistent " +
-            "On all pages the ”Business tax account” service name is contained in the navigation landmark"
+            "On pages ‘Your business details’ and ‘Confirm your email address’ the back options are not consistent " +
+            "On all pages the “Business tax account” service name is contained in the navigation landmark"
 
         }
       }
@@ -336,9 +336,9 @@ class AccessibilityStatementViewSpec extends ViewBaseSpec {
               "not meet Understanding Success Criterion 3.3.2: Labels or Instructions (A) " +
             "On some pages the language selector and back links cannot be skipped using assistive technologies, this " +
               "does not meet Understanding Success Criterion 2.4.1: Bypass Blocks (A) " +
-            "On pages ‘Your business details‘ and ‘Confirm your email address‘ the back options are not consistent, " +
+            "On pages ‘Your business details’ and ‘Confirm your email address’ the back options are not consistent, " +
               "this does not meet Understanding Success Criterion 3.2.3: Consistent Navigation (AA) " +
-            "On all pages the ‘Business tax account‘ service name is contained in the navigation landmark, this does " +
+            "On all pages the “Business tax account” service name is contained in the navigation landmark, this does " +
               "not meet Understanding Success Criterion 1.3.1: Info and Relationships (A)"
         }
       }
