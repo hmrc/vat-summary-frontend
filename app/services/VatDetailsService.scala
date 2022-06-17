fdb30412d10914bbcad0b59c5d19507ff2e2c279
Sheila <63787618+SheilaTran@users.noreply.github.com>
2022-06-17 11:35:58 +0100

    BTAT-10617 addrss PR comments

--- app/services/VatDetailsService.scala
+++ app/services/VatDetailsService.scala
@@ -17,7 +17,6 @@
 package services
 
 import java.time.LocalDate
-
 import config.AppConfig
 import connectors.{FinancialDataConnector, VatObligationsConnector}
 import javax.inject.{Inject, Singleton}
@@ -27,7 +26,6 @@ import models.obligations.Obligation.Status._
 import models.obligations.VatReturnObligations
 import models.payments.{Payments, PaymentOnAccount}
 import uk.gov.hmrc.http.HeaderCarrier
-
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
@@ -51,7 +49,9 @@ class VatDetailsService @Inject()(vatObligationsConnector: VatObligationsConnect
 
     financialDataConnector.getOpenPayments(vrn).map {
       case Right(payments) =>
-        val outstandingPayments = payments.financialTransactions.filter(_.outstandingAmount > 0).filterNot(_.chargeType equals PaymentOnAccount)
+        val outstandingPayments = payments.financialTransactions
+          .filter(_.outstandingAmount > 0)
+          .filterNot(_.chargeType equals PaymentOnAccount)
         if(outstandingPayments.nonEmpty) {
           Right(Some(Payments(outstandingPayments)))
         } else {
