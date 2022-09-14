d95a6969fb4781f4d46d4a685aae725470105d99
paulfirmstone <35600581+paulfirmstone@users.noreply.github.com>
2022-09-14 12:39:19 +0100

    BTAT-10823 Creating view for Estimated LPP1

---
+++ test/views/templates/payments/wyoCharges/EstimatedLPP1ChargeViewSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.templates.payments.wyoCharges
+
+import common.TestModels.estimatedLPP1Model
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import views.ViewBaseSpec
+import views.html.templates.payments.wyoCharges.EstimatedLPP1Charge
+
+class EstimatedLPP1ChargeViewSpec extends ViewBaseSpec {
+
+  val injectedView: EstimatedLPP1Charge = injector.instanceOf[EstimatedLPP1Charge]
+
+  "The estimated LPP1 charge template" should {
+
+    lazy val view = injectedView(estimatedLPP1Model)
+    lazy implicit val document: Document = Jsoup.parse(view.body)
+
+    "have the correct charge description text" in {
+      elementText("a") shouldBe "Penalty for late payment of VAT for period 1 Jan to 2 Feb 2020"
+    }
+
+    "not have an overdue label" in {
+      elementExtinct(".govuk-tag")
+    }
+
+    "have the correct due hint text" in {
+      elementText("span") shouldBe "estimate"
+    }
+
+    "have a link with the correct href" in {
+      element("a").attr("href") shouldBe
+        testOnly.controllers.routes.ChargeBreakdownController.showBreakdown(
+          estimatedLPP1Model.generateHash(user.vrn)
+        ).url
+    }
+  }
+}
