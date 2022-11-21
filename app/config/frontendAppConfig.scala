82ae4123d3278fd80d75f6b9a68ac004b3accf46
paulfirmstone <35600581+paulfirmstone@users.noreply.github.com>
2022-11-21 18:26:43 +0000

    BTAT-11045 Updating crystallised interest view content

--- app/config/frontendAppConfig.scala
+++ app/config/frontendAppConfig.scala
@@ -99,6 +99,7 @@ trait AppConfig {
   val penaltiesFrontendUrl: String
   val fixEmailUrl: String
   val govUkHoldingUrl: String
+  val govUkPrevIntRateUrl: String
   val timeToLiveInSeconds: Int
 }
 
@@ -247,6 +248,8 @@ class FrontendAppConfig @Inject()(val runModeConfiguration: Configuration, sc: S
 
   override lazy val penaltiesFrontendUrl: String = sc.getString(Keys.penaltiesFrontendHost) + sc.getString(Keys.penaltiesFrontendUrl)
   override val govUkHoldingUrl: String = sc.getString(Keys.govUkHoldingUrl)
+
+  override lazy val govUkPrevIntRateUrl: String = sc.getString(Keys.govUkPrevIntRateUrl)
 
   override lazy val timeToLiveInSeconds: Int = sc.getInt(Keys.timeToLive)
 }
